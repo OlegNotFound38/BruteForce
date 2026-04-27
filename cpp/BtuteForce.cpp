80cf40312583ea542fe080d37d41e7b169c38053
OlegNotFound38 <pronkinoa@gmail.com>
2026-04-27 15:55:05 +0300

    simbols list

---
+++ cpp/BtuteForce.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string simbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; // posible simbols
+
+    cout << "Enter Password:\n";
+    string password; cin >> password;
+    
+    
+    return 0;
+}
