c3f7ec9e21498eec39c5a406ba14b73353a3ba96
OlegNotFound38 <pronkinoa@gmail.com>
2026-04-27 16:57:24 +0300

    +- structure

--- cpp/BtuteForce.cpp
+++ cpp/BtuteForce.cpp
@@ -4,11 +4,19 @@
 using namespace std;
 
 int main(){
-    string simbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; // posible simbols
-
-    cout << "Enter Password:\n";
-    string password; cin >> password;
+    const string SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; // ⁡⁢⁢⁢posible symbols⁡ ⁡⁢⁢⁣now 62⁡
+    const int SYMBOL_COUNT = SYMBOLS.length();
     
+    cout << "Enter Password:\n"; string password; cin >> password;
+    cout << "Enter the maximum password length:\n"; int password_length; cin >> password_length;
+
+    while (password_length > 0){
+        for (int i = 0; i < password_length; i++){
+            
+        }
+
+        password_length--;
+    }
     
     return 0;
 }
