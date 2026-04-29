8142998f133082f3bf426a1f54eb031e3c9a7af5
OlegNotFound38 <pronkinoa@gmail.com>
2026-04-29 23:09:12 +0300

    working model, but this write not me

--- cpp/BtuteForce.cpp
+++ cpp/BtuteForce.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -9,14 +10,35 @@ int main(){
     
     cout << "Enter Password:\n"; string password; cin >> password;
     cout << "Enter the maximum password length:\n"; int password_length; cin >> password_length;
-
-    while (password_length > 0){
-        for (int i = 0; i < password_length; i++){
-            
+    cout << "Enter the minimum password length:\n"; int min_password_length; cin >> min_password_length;
+    string res="";
+    char m[10]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+    bool flag=false;
+    do{
+        if(flag)
+        {
+            res="";
+            for(int i=0;i<password_length;i++)
+            {
+                if(m[i]>=0)
+                    res=res+SYMBOLS[m[i]];
+                else break;
+            }
+            flag=false;
+        }else res[0]=SYMBOLS[m[0]];
+        if(res==password)
+        {
+            cout << res << endl;
+            return 0;
         }
-
-        password_length--;
-    }
-    
-    return 0;
+        for(int i=0;i<password_length;i++)
+        {
+            m[i]=m[i]+1;
+            if(m[i]<SYMBOL_COUNT)break;
+            m[i]=0;
+            flag=true;
+        }
+    }while(password_length > min_password_length);
+    cout << "Password Not Found... like Oleg//\n good luck" << endl;
+    return 1;
 }
